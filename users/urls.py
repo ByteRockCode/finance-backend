0cd9e99a341c161595cfaf276c28e36a65238dbc
Saul Hormazabal <saul.hormazabal@gmail.com>
2018-06-26 23:22:26 -0400

    User profile

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import signup
+from .views import UserUpdateView
 
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('profile/', UserUpdateView.as_view(), name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.views.generic.edit import UpdateView
+
+
+User = get_user_model()
 
 
 def signup(request):
@@ -21,3 +27,17 @@ def signup(request):
         return redirect('index')
 
     return render(request, 'users/signup.html', {'form': form})
+
+
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    ]
+    template_name = 'users/user_form.html'
+
+    def get_object(self):
+        return self.request.user
