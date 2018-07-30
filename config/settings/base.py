c8a3aac53f26efc451fa66fe8786281110b54928
Saul Hormazabal <saul.hormazabal@gmail.com>
2018-07-30 17:57:36 -0400

    Email settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -163,3 +163,11 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Google Analytics
 GOOGLE_ANALYTICS_ID = os.environ.get('GOOGLE_ANALYTICS_ID')
+
+
+# Email
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 25)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', True)
